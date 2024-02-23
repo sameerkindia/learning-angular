1ec3c669ad9e0eb0fc57f55ac86afa98c6987d2f
sameer khan <sameerkhanaa5@gmail.com>
2024-02-23 19:57:14 +0530

    adding login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CartComponent } from './cart/cart.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, CartComponent],
+  imports: [RouterOutlet, HomeComponent, CartComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,12 +1,53 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './cart.component.html',
-  styleUrl: './cart.component.css'
+  styleUrl: './cart.component.css',
 })
 export class CartComponent {
+  componentName = 'Cart Component';
+  btnDisabled = true;
+  isLogin = false;
+  userName = '';
+  password = '';
+  item = '';
+  cart: any = [];
+  bg = 'gray';
 
+  constructor() {
+    setTimeout(() => {
+      this.btnDisabled = false;
+    }, 2000);
+  }
+
+  // getItem(e: any) {
+  //   this.item = e.target.value;
+  // }
+
+  addToCart() {
+    this.cart.push(this.item);
+    this.item = '';
+  }
+
+  getUserName(e: any) {
+    this.userName = e.target.value;
+  }
+  getPassword(e: any) {
+    this.password = e.target.value;
+  }
+
+  login() {
+    this.isLogin = true;
+  }
+
+  logout() {
+    this.isLogin = false;
+    this.userName = '';
+    this.password = '';
+  }
 }
